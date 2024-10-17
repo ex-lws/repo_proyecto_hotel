9a84a218498e1cb33e73fc8a542a6b377c8adac0
Luis Alberto Reyes Cruz <theleroybottom@gmail.com>
2024-10-17 07:55:41 -0600

    Segunda versión de menú.
    Incluye mejor modulación y más llamadas a las funciones necesarias.

---
+++ Modulo 0 - Menu principal/menuV1.1.py
@@ -0,0 +1,19 @@
+#Menu de consola apra la gestión del hotel Kaligula 
+#Versión 1.0
+#Fecha: 04/10/2024
+#Autor: Reyes Cruz Luis Alberto
+
+#Correciones: 
+
+def mostrarOpciones():
+
+    print("====================================")
+    print("Bienvenido al sistema de gestión de hoteles")
+    print("====================================")
+    print ("0.- Mostrar ayuda")
+    print("1.- Mostrar todas las habitaciones")
+    print ("2.- Mostrar estado de disponibilidad de las habitaciones")
+    print("3.- Inicar proceso de reservación")
+    print("4.- Consultar reportes de reservación")
+    print ("5.- Estadísticas operacionales")
+    print("6.- Salir")
