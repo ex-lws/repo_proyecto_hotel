eff62ac9464bea3db7d0a892b19704ebe5122c00
Luis Alberto Reyes Cruz <theleroybottom@gmail.com>
2024-10-17 07:43:35 -0600

    Primera versión del menú de selcción.
    Esta clase tiene los modulos que corresponden al menú principal del proyecto.

---
+++ Modulo 0 - Menu principal/menuV1.0.py
@@ -0,0 +1,18 @@
+#Menu de consola apra la gestión del hotel Kaligula 
+#Versión 1.0
+#Fecha: 04/10/2024
+#Autor: Reyes Cruz Luis Alberto
+
+#Correciones: 
+
+def mostrarOpciones():
+
+    print("============")
+    print("Bienvenido al sistema de gestión de hoteles")
+    print("============")
+    print("1.- Mostrar todas las habitaciones")
+    print("2.- Reservar una habitación")
+    print("3.- Consultar reportes de reservación")
+    print("4.- Salir")
+
+mostrarOpciones()
